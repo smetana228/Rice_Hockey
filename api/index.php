fdf081fbd2d745a1de590539601d2a23f0256f41
david-nyari <david.e.nyari@gmail.com>
2026-03-03 12:00:51 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <meta name="description" content="Rice Hockey Club">
 <meta name="google-site-verification" content="https://github.com/smetana228"/>
-<?php include 'lib/module/sys-meta.php';?>
+<?php include '../lib/module/sys-meta.php';?>
 <meta property="og:title" content="Rice Hockey Club" />
 <meta property="og:description" content="Website for Rice Hockey Club" />
 <meta property="og:image:width" content="1200" />
@@ -15,8 +15,8 @@
 <meta property="og:type" content="website" />
 <meta property="og:site_name" content="Rice Hockey Club" />
 
-<?php include 'lib/module/sys-css.php';?>
-<?php include 'lib/module/sys-js.php';?>
+<?php include '../lib/module/sys-css.php';?>
+<?php include '../lib/module/sys-js.php';?>
 <script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
 </head>
@@ -26,7 +26,7 @@
 <img src="/img/videos/1.jpg" width="1" height="1" alt="Preload of highlight 1"/>
 <img src="/img/videos/2.jpg" width="1" height="1" alt="Preload of highlight 2"/>
 <img src="/img/videos/3.jpg" width="1" height="1" alt="Preload of highlight 3"/>
-<?php include 'lib/module/sys-php.php';?>
+<?php include '../lib/module/sys-php.php';?>
 
 <div class="page-con-content landing-con-override">
 	<div class="wavebar-con-container-master">
@@ -76,11 +76,12 @@
 				</div>
 				<div class="video-con-divider">
 				</div>
-				<?php include 'lib/module/inc-video-carousel.php';?>
+				<?php include '../lib/module/inc-video-carousel.php';?>
 			</div>
 		</div>
 	</div>
-	<?php include 'lib/module/inc-attribution.php';?>
+	<?php include '../lib/module/inc-attribution.php';?>
 	</div>
 </body>
 </html>
+
