6c787910ac10c3eb7d4aef030ec530eea6872b5f
smetana228 <greatm350@gmail.com>
2026-03-12 17:38:31 -0500

    Debug

--- lib/module/inc-attribution.php
+++ lib/module/inc-attribution.php
@@ -1,7 +1,7 @@
 <div class="site-attribution" style="font-family: Roboto-Regular; font-size: 13px; color: #666; text-align: center; padding: 20px 0;">
 <p>
 © 2026 Rice University.<br>
-This website is licensed under the <a href = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html" target = "_blank">GNU General Public License v2.0. </a><br>
+This website is licensed under the <a href = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html" target = "_blank">GNU GPL v2.0. </a><br>
 Made by <a href="https://github.com/smetana228" target="_blank">smetana228</a>
 </p>
 </div>
